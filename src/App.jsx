98cd76be55bb67e00c558d1458c3b726ebe43b4e
Dieguit0000 <diegofernandomorenomartinez@gmail.com>
2025-03-20 07:37:41 -0500

    Configuración inicial tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-import { useState } from 'react';
-import './App.css';
-import Dashboard from './Componentes/Dashboard';
+import { useState } from 'react'
+import "./index.css"
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="text-center">
-      <header className="bg-blue-500 text-white p-4">
-        <h1 className="text-4xl font-bold">Bienvenido a INVET++</h1>
-      </header>
-      <Dashboard />
-      <div className="card mt-4">
-        <button 
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </button>
-        <p className="mt-4">
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-    </div>
-  );
+    <>
+      <h1 className='text-2xl font-bold text-center bg-red-500'>
+        Hellow world!!
+      </h1>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="bg-red-500">
+        <div className="container">
+            <a className="navbar-brand" href="/">
+            Navbar
+            </a>
+        </div>
+        </nav>
+    );
+}
+
+export default Navbar;
